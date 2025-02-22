887326fbe33f60531b27f7cd7c82ac5ec8b5e144
Fabri0607UNA <fabricio.alfaro.arce@est.una.ac.cr>
2025-02-22 15:52:18 -0600

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import AgregarEmpleado from "./empleados/AgregarEmpleado";
+import Navegacion from "./plantilla/navegacion.js";
+import EditarEmpleado from "./empleados/EditarEmpleado.js";
+
+function App() {
+  return (
+
+    <div className="container"> 
+      <BrowserRouter>
+        <Navegacion/>
+        <Routes>
+          <Route exact path="/" element={<ListadoEmpleados/>}/>
+          <Route exact path="/agregar" element={<AgregarEmpleado/>}/>
+          <Route exact path="/editar/:id" element={<EditarEmpleado/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+    
+  );    
+}  
+
+export default App;
